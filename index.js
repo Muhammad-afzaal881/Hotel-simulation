c8661450ede2194a1190228ab624e0befdd5aeb3
malikafzaalali5@gmail.com <malikafzaalali5@gmail.com>
2025-06-19 10:19:48 +0500

    update

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ let q4 =document.getElementById("q4")
 let q5 =document.getElementById("q5")
 let q6 =document.getElementById("q6")
 let q7 =document.getElementById("q7")
+let q8 =document.getElementById("q8")
+let bttn =document.getElementById("bttn")
 let btn =document.getElementById("btn")
+let button =document.getElementById("button")
+let Payment_card =document.getElementById("Payment")
 const orderBox = document.getElementById('orderBox');
 const orderDetails = document.getElementById('orderDetails');
 
 function main(){
     landing_page.style.display ="none"
     Questionpage.style.display = "block"
+     q1.style.display = "block"
 }
 
 function like(){
@@ -110,4 +115,44 @@ function orderpass(){
         order_card.style.display ="none"
         btn.style.display ="block"
      }
+}
+function askAgain() {
+    orderBox.style.display = 'none';
+    order_card.style.display = 'block';
+    q8.style.display = "none"
+
+}
+function pay(){
+    order_card.style.display ="none"
+    Payment_card.style.display ="block"
+    btn.style.display ="none"
+    orderBox.style.display ="none"
+     q8.style.display = "none"
+}
+
+function suggestion(){
+         q1.style.display = "none"
+    q2.style.display = "none"
+    q3.style.display = "none"
+    q4.style.display = "none"
+    q5.style.display = "none"
+    q6.style.display = "none"
+    q7.style.display = "none"
+    q8.style.display = "block"
+    menu_card.style.display ="none"
+    order_card.style.display ="none"
+    Payment_card.style.display ="none"
+    btn.style.display ="none"
+    orderBox.style.display ="none"
+}
+let billsession = document.getElementById("bill-container")
+function bill(){
+   alert("Payment recived succesfull")
+   Payment_card.style.display= "none"
+   landing_page.style.display ="block"
+}
+function main(){
+    landing_page.style.display ="none"
+    Questionpage.style.display = "block"
+     q1.style.display = "block"
 }
